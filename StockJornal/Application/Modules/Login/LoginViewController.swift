69a8082b49a48be92a5543fa380a781c67e6c8d2
Gaberu <gabrielrosadn@gmail.com>
2020-12-21 10:46:41 -0300

    Add login view as main view

---
+++ StockJornal/Application/Modules/Login/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  StockJornal
+//
+//  Created by Gabriel Rosa on 12/21/20.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Estou no login")
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
